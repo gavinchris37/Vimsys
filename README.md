c9d9654d9a1b15469a85cfcbf4e5500619f5f04d
GavinChris7 <gavchris.37@gmail.com>
2023-01-31 06:25:12 +0530

    CRUD Operations

--- README.md
+++ README.md
@@ -18,53 +18,59 @@ Developers who have experience with current technologies and contributed to proj
 - Django
 - Machine Learning and AI
 - CI/CD pipelines
-- Automate Workflows 
+- Automate Workflows
 - Setup Environments
 
-
 ## Pip List
-Package            Version
------------------- ---------
-asgiref            3.6.0
-autopep8           1.6.0
-bleach             3.1.0
-certifi            2022.12.7
-cycler             0.10.0
-defusedxml         0.5.0
-Django             3.2.16
-djongo             1.3.6
-dnspython          2.3.0
-entrypoints        0.3
-ipywidgets         7.4.2
-jsonschema         3.0.1
-jupyter            1.0.0
-jupyter-console    6.0.0
-kiwisolver         1.0.1
-matplotlib         3.0.3
-mistune            0.8.4
-mpmath             1.1.0
-nbconvert          5.4.1
-nbformat           4.4.0
-nose               1.3.7
-notebook           5.7.8
-pandocfilters      1.4.2
-pip                22.3.1
-prometheus-client  0.6.0
-pycodestyle        2.10.0
-pyparsing          2.3.1
-pyrsistent         0.14.11
-pytz               2022.7.1
-pywinpty           0.5.5
-qtconsole          4.4.3
-Send2Trash         1.5.0
-setuptools         65.6.3
-sqlparse           0.2.4
-sympy              1.3
-terminado          0.8.2
-testpath           0.4.2
-toml               0.10.2
-typing_extensions  4.4.0
-webencodings       0.5.1
-wheel              0.37.1
+
+Package Version
+
+---
+
+asgiref 3.6.0
+autopep8 1.6.0
+bleach 3.1.0
+certifi 2022.12.7
+cycler 0.10.0
+defusedxml 0.5.0
+Django 3.2.16
+djongo 1.3.6
+dnspython 2.3.0
+entrypoints 0.3
+ipywidgets 7.4.2
+jsonschema 3.0.1
+jupyter 1.0.0
+jupyter-console 6.0.0
+kiwisolver 1.0.1
+matplotlib 3.0.3
+mistune 0.8.4
+mpmath 1.1.0
+nbconvert 5.4.1
+nbformat 4.4.0
+nose 1.3.7
+notebook 5.7.8
+pandocfilters 1.4.2
+pip 22.3.1
+prometheus-client 0.6.0
+pycodestyle 2.10.0
+pyparsing 2.3.1
+pyrsistent 0.14.11
+pytz 2022.7.1
+pywinpty 0.5.5
+qtconsole 4.4.3
+Send2Trash 1.5.0
+setuptools 65.6.3
+sqlparse 0.2.4
+sympy 1.3
+terminado 0.8.2
+testpath 0.4.2
+toml 0.10.2
+typing_extensions 4.4.0
+webencodings 0.5.1
+wheel 0.37.1
 widgetsnbextension 3.4.2
-wincertstore       0.2
+wincertstore 0.2
+
+### Important Notes:
+
+    {{ form.as_p }}
--- vserver/projects/views.py
+++ vserver/projects/views.py
@@ -1,28 +1,85 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-projectList = [
+from .forms import ProjectForm
+from .models import Project
 
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional Ecommerce website'
-    },
 
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
 
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    },
 
-]
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    #tags = projectObj.tags.all()
+    print('projectObj :', projectObj)
+    return render(request, 'projects/project.html', {'project': projectObj})
 
+    # return render(request, 'projects/project.html', {'project': projectObj, 'tags': tags})
+
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        # print(request.POST)
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        # print(request.POST)
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)
+
+
+# projectList = [
+#
+#    {
+#        'id': '1',
+#        'title': 'Ecommerce Website',
+#        'description': 'Fully functional Ecommerce website'
+#    },
+#
+#    {
+#        'id': '2',
+#        'title': 'Portfolio Website',
+#        'description': 'A personal website to write articles and display work'
+#    },
+#
+#    {
+#        'id': '3',
+#        'title': 'Social Network',
+#        'description': 'An open source project built by the community'
+#    },
+#
+# ]
+#
 # projectList = [
 #    {
 #        'id': '1',
@@ -40,21 +97,3 @@ projectList = [
 #        'description': 'Fully functional Social website',
 #    },
 # ]
-
-
-def projects(request):
-    msg = 'Hello you are in the projects page'
-    page = 'projects'
-    number = 10
-    context = {'page': page, 'number': number,
-               'projects': projectList}
-    return render(request, 'projects/projects.html', context)
-
-
-def project(request, pk):
-    projectObj = None
-    for project in projectList:
-        if project['id'] == pk:
-            projectObj = project
-            # break
-    return render(request, 'projects/project.html', {'project': projectObj})

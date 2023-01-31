f05b79fbdc1fd47c277f98cda981e4b86da7cd6c
GavinChris7 <gavchris.37@gmail.com>
2023-02-01 03:49:10 +0530

    importing theme

--- vserver/projects/models.py
+++ vserver/projects/models.py
@@ -8,6 +8,8 @@ class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           db_index=True, primary_key=True, editable=False)
     title = models.CharField(max_length=200)
+    featured_image = models.ImageField(
+        null=True, blank=True, default="default.jpg")
     description = models.TextField(null=True, blank=True, max_length=600)
     #demo_link = models.URLField(null=True, blank=True, max_length=2000)
     demo_link = models.CharField(null=True, blank=True, max_length=2000)
--- vserver/vserver/settings.py
+++ vserver/vserver/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y5v&(dz+9p0o+xd)61-4sgdvg6f_$^+voh&+2x$@5u-na4!#22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +127,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
